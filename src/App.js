39c370ec04ea5d0ec89e9756c2e1fb80cceb613a
Mann Mehta <mannmehta2199@gmail.com>
2022-11-26 18:05:10 +0530

    Bootstrap the project with route, redux and material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Switch>
+        <Route component={Welcome} path="/welcome" exact />
+        <Route component={Home} path="/home" exact />
+        <Route exact path="/*">
+          <Redirect to="/Welcome" />
+        </Route>
+      </Switch>
     </div>
   );
 }
